3e378c43d11a463538cd881306ac5b4bb57a8587
TheGreatAutomata <3368925785@qq.com>
2021-11-23 15:26:03 +0800

    debug for resource not found

---
+++ explorer/src/main/java/com/dnielfe/manager/MyShowActivity.java
@@ -0,0 +1,14 @@
+package com.dnielfe.manager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//import com.dnielfe.R;
+
+public class MyShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_show);
+    }
+}
